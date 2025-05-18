5271b17169ba8b31ed2b1b0b5ec2494d8462bf48
andreiibl <andrei.balasoiu03@gmail.com>
2025-05-18 16:18:35 +0200

    ENH: Bloqueo para girar la pantalla

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; 
 import 'screens/login_screen.dart';
 
-void main() 
-{
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(MyApp());
 }
 
